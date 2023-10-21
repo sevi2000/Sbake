da419da3909c3b040ed989bdf2706caef6c2823d
AOUINE Katia <katia.aouine2000@gmail.com>
2023-10-21 15:56:11 +0200

    fixed bounderies and sbake movement.

--- core/src/com/mygdx/game/Field.java
+++ core/src/com/mygdx/game/Field.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 
 
 import java.util.*;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.g2d.Batch;
 public class Field {
@@ -12,6 +13,8 @@ public class Field {
     int height;
     Map<FieldElement, Vector2> field;
     BodyPart lastAddedPart;
+    long timeForNextMove;
+    static final long TIME_FOR_NEXT_MOVE_MAX = 200;
     Field() {
         field = new HashMap<>();
         this.width = 10;
@@ -37,34 +40,38 @@ public class Field {
         updateField();
     }
     public void updateField(){
-        for (FieldElement elt:field.keySet()){
-            System.out.println("updating body");
-            if (elt instanceof BodyPart){
-                switch (((BodyPart)elt).direction) {
+        timeForNextMove += Gdx.graphics.getDeltaTime() * 1000;
+        if (timeForNextMove > TIME_FOR_NEXT_MOVE_MAX) {
+            timeForNextMove -= TIME_FOR_NEXT_MOVE_MAX;
+        for (FieldElement elt:field.keySet()) {
+            if (elt instanceof BodyPart elt1) {
+                switch (elt1.direction) {
                     case UP:
-                        elt.setY(field.get(elt).y++);
+                        if (field.get(elt1).y != 9)
+                            elt1.setY(field.get(elt1).y++);
+                        elt1.direction = Direction.UP;
                         break;
                     case DOWN:
-                        elt.setY(field.get(elt).y--);
+                        if (field.get(elt1).y != 0)
+                            elt1.setY(field.get(elt1).y--);
+                        elt1.direction = Direction.DOWN;
                         break;
                     case LEFT:
-                        elt.setY(field.get(elt).x--);
+                        if (field.get(elt1).x != 0)
+                            elt1.setY(field.get(elt1).x--);
+                        elt1.direction = Direction.LEFT;
                         break;
                     case RIGHT:
-                        elt.setY(field.get(elt).x++);
+                        if (field.get(elt1).x != 9)
+                            elt1.setY(field.get(elt1).x++);
+                        elt1.direction = Direction.RIGHT;
                         break;
                 }
-                        try {
-                            Thread.sleep(300);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+            }
 
-            }
-            if (field.get(elt).x != 0 && field.get(elt).y != 0) {
-                elt.setX(field.get(elt).x * 50);
-                elt.setY(field.get(elt).y * 50);
-            }
+            elt.setX(field.get(elt).x * 50);
+            elt.setY(field.get(elt).y * 50);
+        }
         }
     }
 }
