95fd4933ae96186607561695e35d413b41fd7b0d
Sevi Dervishi <sevidervishi@outlook.fr>
2023-11-01 20:50:43 +0100

    Added Textures

--- core/src/com/mygdx/game/BodyPart.java
+++ core/src/com/mygdx/game/BodyPart.java
@@ -2,17 +2,24 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
 
 public class BodyPart extends FieldElement {
     Color color = Color.GREEN;
     Direction direction = Direction.UP;
+    Texture texture;
     BodyPart(Batch batch) {
         super(batch);
+        texture = new Texture(Gdx.files.internal("images/head.jpg"));
     }
     @Override
     public void draw(Batch btc, float transparency) {
         this.shDr.setColor(color);
         this.shDr.filledRectangle(getX(),getY(),getWidth(),getHeight());
+        btc.draw(texture,this.getX(),this.getY(),getWidth(),getHeight());
     }
 
     public void setDirection(Direction direction) {
--- core/src/com/mygdx/game/Donut.java
+++ core/src/com/mygdx/game/Donut.java
@@ -4,21 +4,27 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import space.earlygrey.shapedrawer.ShapeDrawer;
-
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
 
 public class Donut extends FieldElement {
     Color color = Color.RED;
     boolean special;
+    Texture texture;
     public Donut(Batch batch, boolean special){
         super(batch);
         this.special = special;
         if (special)
             color = Color.ORANGE;
+        texture = new Texture(Gdx.files.internal("images/donut.jpg"));
     }
     @Override
     public void draw(Batch btc, float transparency) {
         this.shDr.setColor(color);
-        this.shDr.filledRectangle(getX(),getY(),getWidth(),getHeight());
+        //this.shDr.filledRectangle(getX(),getY(),getWidth(),getHeight());
+        btc.draw(texture,this.getX(),this.getY(),getWidth(),getHeight());
     }
     public int score(){
         return special? 6:1;
