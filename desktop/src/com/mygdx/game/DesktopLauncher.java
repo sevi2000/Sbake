d4693863e01044ca35cfc3fd7babe701a91964a8
Sevi Dervishi <sevidervishi@outlook.fr>
2023-11-01 14:27:18 +0100

    Fixed window size.

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowedMode(500,500);
+		config.setWindowedMode(20 * 27, 20 * 27);
 		
 		config.setTitle("Sbake");
 		new Lwjgl3Application(new SbakeGame(), config);
